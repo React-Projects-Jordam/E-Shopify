594e552840758376262bfad40b2e6fb6e26200b5
omaraldareh <aldarehomar@gmail.com>
2026-04-08 02:48:44 +0300

    fix: make header responsive and fix z-index issues

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,22 +3,22 @@ import { Link } from 'react-router-dom'
 
 const Navbar = ({ className }) => {
   return (
-    <div className={`flex text-white justify-center gap-40 text-2xl ${className}`}>
+    <div className={`flex flex-col lg:flex-row items-center text-white gap-6 lg:gap-16 text-lg lg:text-2xl ${className}`}>
       
-      <Link to='/'>
-        <div className='cursor-pointer font-bold font-sans hover:bg-sky-700 p-5 rounded-lg'>
+      <Link to='/' className='w-full lg:w-auto'>
+        <div className='text-center lg:text-left cursor-pointer font-bold hover:bg-sky-700 p-3 rounded-lg'>
           Home
         </div>
       </Link>
 
-      <Link to='/products'>
-        <div className='cursor-pointer font-bold font-sans hover:bg-sky-700 p-5 rounded-lg'>
+      <Link to='/products' className='w-full lg:w-auto'>
+        <div className='text-center lg:text-left cursor-pointer font-bold hover:bg-sky-700 p-3 rounded-lg'>
           Products
         </div>
       </Link>
 
-      <Link to='/about'>
-        <div className='cursor-pointer font-bold font-sans hover:bg-sky-700 p-5 rounded-lg'>
+      <Link to='/about' className='w-full lg:w-auto'>
+        <div className='text-center lg:text-left cursor-pointer font-bold hover:bg-sky-700 p-3 rounded-lg'>
           About
         </div>
       </Link>
